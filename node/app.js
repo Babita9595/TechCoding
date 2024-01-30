22d5f67041e8a80d35587c613dd73b50c3cf5a22
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-01-30 21:07:13 +0530

    implementing cors

--- node/app.js
+++ node/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 app.use(express.json());
+
+// GLOBAL MIDDLEWARES
+//Implementing CORS
+app.use(cors());
 
 app.use("/api/v1/users", userRouter);
 
