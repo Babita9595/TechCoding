4585ffcfcacf9c52eef9a566e073cc289e066d28
babita koushik <babitakoushik@gmail.com>
2024-02-18 12:36:27 +0530

    basic design

--- react/src/components/Carousel/Carousel.jsx
+++ react/src/components/Carousel/Carousel.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import image1 from '../../../src/assets/carouselImage/a1.jpg'
+import image2 from '../../../src/assets/carouselImage/a2.jpg'
+import image3 from '../../../src/assets/carouselImage/a3.jpg'
+
 
 const CarouselImage = () => {
-    useEffect(() => {
-    }, []);
+
 
     return (
         <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">
-                    <img src="https://i.ibb.co/Hp0Z8Sd/stydyimg.jpg" height='500' className="d-block w-100" alt="..." />
+                    <img src={image1} height='500' className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="https://i.ibb.co/Hp0Z8Sd/stydyimg.jpg" height='500' className="d-block w-100" alt="..." />
+                    <img src={image2} height='500' className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="https://i.ibb.co/Hp0Z8Sd/stydyimg.jpg" height='500' className="d-block w-100" alt="..." />
+                    <img src={image3} height='500' className="d-block w-100" alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
