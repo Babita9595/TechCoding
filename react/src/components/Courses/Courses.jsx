ff2bc8922f4a472e90ffddbd92046ab4974b5c5b
babita koushik <babitakoushik@gmail.com>
2024-03-03 12:40:10 +0530

    fetching data from api

--- react/src/components/Courses/Courses.jsx
+++ react/src/components/Courses/Courses.jsx
@@ -7,19 +7,46 @@ import axios from 'axios'
 
 const Courses = () => {
 
-    const [courses, setCourses] = useState()
+    const [courses, setCourses] = useState({})
 
-    // useEffect(() => {
-    //     axios.get(`http://localhost:9000/api/v1/courses`)
-    //         .then((res) => console.log(res))
-    //         .catch((err) => console.log(err))
-    // }, [])
+    useEffect(() => {
+        axios.get(`http://localhost:9000/api/v1/courses`)
+            .then((res) => {
+                setCourses(res?.data)
+                console.log(res?.data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className='main-card-container'>
             <h3> Software Training Courses Provided By Tech-Coding -</h3>
             <div className='card-container d-flex flex-wrap'>
-                <div className="card">
+                {
+                    courses?.courses?.map((carditem) =>
+                        <div className="card">
+                            <div className="property-card">
+                                <a href="#">
+                                    <div className="property-image">
+                                        <div className="property-image-title">
+                                            image
+                                        </div>
+                                    </div></a>
+                                <div className="property-description">
+                                    <h5> {carditem?.name}</h5>
+                                    <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                                </div>
+                                <a href="#">
+                                    <div className="property-social-icons">
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    )
+
+                }
+                {/* <div className="card">
                     <div className="property-card">
                         <a href="#">
                             <div className="property-image">
@@ -37,28 +64,9 @@ const Courses = () => {
                             </div>
                         </a>
                     </div>
-                </div>
-                <div className="card">
-                    <div className="property-card">
-                        <a href="#">
-                            <div className="property-image">
-                                <div className="property-image-title">
+                </div> */}
 
-                                </div>
-                            </div></a>
-                        <div className="property-description">
-                            <h5> Card Title </h5>
-                            <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
-                        </div>
-                        <a href="#">
-                            <div className="property-social-icons">
-
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="card">
+                {/* <div className="card">
                     <div className="property-card">
                         <a href="#">
                             <div className="property-image">
@@ -135,7 +143,7 @@ const Courses = () => {
                         </div>
 
                     </div>
-                </div>
+                </div> */}
 
             </div>
 
