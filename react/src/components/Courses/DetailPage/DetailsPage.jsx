f360f19960965ace722ecd6a649a2b96f38b082a
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-03-09 09:29:23 +0530

    DetailsPage

--- react/src/components/Courses/DetailPage/DetailsPage.jsx
+++ react/src/components/Courses/DetailPage/DetailsPage.jsx
@@ -1,35 +1,52 @@
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
 // import detailPage from "../../../../src/assets/courses/tech-img.jpg"
-import detailImage from "../../../assets/courses/tech-img.jpg"
-
+import detailImage from "../../../assets/courses/tech-img.jpg";
+import "./DetailsPage.css";
 
 const DetailsPage = () => {
-    return (
-        <div>
-            <Header />
+  return (
+    <div>
+      <Header />
 
-            <div className="row">
-                <div className="col-6">
-                    <div className="card" style={{ width: "500px", height: "600px" }}>
-                        <img className="card-img-top" height="600px" src={detailImage} alt="Card image cap" />
-                    </div>
-                </div>
-                <div className="col-6">
-                    <div>
-                        <h4>Course Title</h4>
-                        <p>course description</p>
-                    </div>
-                    <div>
-                        <span>rating</span> <span>reviews</span>
-                        <p>Pricing</p>
-                    </div>
-                    <button>payment </button>
-                </div>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={detailImage}
+                alt="Course Preview"
+              />
             </div>
-            <Footer />
+          </div>
+          <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <div className="details-content text-center">
+              <h2 className="course-title">Mastering React Development</h2>
+              <p className="course-description">
+                Dive deep into the world of React development and build
+                powerful, scalable web applications with this comprehensive
+                course. Whether you are a beginner or an experienced developer,
+                you will learn the best practices and advanced techniques to
+                master React.
+              </p>
+
+              <div className="ratings">
+                <span className="rating">4.8</span>
+                <span className="reviews">120 reviews</span>
+              </div>
+
+              <p className="pricing">Pricing: $49.99</p>
+
+              <button className="btn btn-primary">Enroll Now</button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default DetailsPage;
